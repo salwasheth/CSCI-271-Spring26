e4bc10d9bf7e2ea81428a8d2efff9ef06c9acb60
salwasheth <salwa.sheth@jjay.cuny.edu>
2026-02-01 23:23:26 -0500

    Add files via upload

---
+++ Week-1/code.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+    std::cout << "Hello, My name is Salwa!" << std::endl;
+    std::cout << "I am a freshman at John Jay College of Criminal Justice" << std::endl;
+    std::cout << "I am majoring in Computer Sceince & Information Security" <<std::endl;
+    return 0;
+}
